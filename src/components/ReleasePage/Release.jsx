fc2054cf8d642f0868b48d227012807e1a092e81
Jenda <jan.illetsko@gmail.com>
2023-03-10 16:03:24 +0100

    styling

--- src/components/ReleasePage/Release.jsx
+++ src/components/ReleasePage/Release.jsx
@@ -3,26 +3,24 @@ import { useEffect, useState } from "react";
 const Release = ({ release }) => {
   const image = `https://coverartarchive.org/release/${release.id}/front`;
   return (
-    <div className="container">
-      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3">
-        <div className="col mb-4">
-          <div className="card h-100 w-50 p-3">
-            <div className="card-body">
-              <h3 className="card-title">{release.title}</h3>
-              <div className="card-text">
-                <p>{release.date}</p>
-                <p>{release.packaging}</p>
-                <p>
-                  {release["text-representation"].script} -{" "}
-                  {release["text-representation"].language}
-                </p>
-              </div>
-              <img
-                className="card-img-top"
-                style={{ width: "300px" }}
-                src={image}
-                alt="No image"
-              />
+    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 justify-content-center">
+      <div className="col mb-4">
+        <div className="card h-100">
+          <img
+            className="card-img-top"
+            src={image}
+            alt="No image"
+            style={{ height: "500px", objectFit: "cover" }}
+          />
+          <div className="card-body d-flex flex-column justify-content-center align-items-center">
+            <h3 className="card-title text-center">{release.title}</h3>
+            <div className="card-text text-center">
+              <p>{release.date}</p>
+              <p>{release.packaging}</p>
+              <p>
+                {release["text-representation"].script} -{" "}
+                {release["text-representation"].language}
+              </p>
             </div>
           </div>
         </div>
