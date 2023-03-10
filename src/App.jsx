026503a9571e0f4ee51598eace7e302be1216e5f
Mario <lartu@seznam.cz>
2023-03-10 09:26:26 +0100

    ds-store included into gitignore plus simple change

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ import "./App.scss";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>testing ds</h1>
+    </div>
+  );
 }
 
 export default App;
